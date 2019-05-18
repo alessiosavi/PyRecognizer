300da58ae889e085635bf73ac5a69f5a20177f2e
AlessioSavi <alessio.savi-cic-it@ibm-com>
2019-05-18 15:08:03 +0200

    Enhancements - Add flask dashboard for track API performance/usage - Moved log in a dedicated folder
    Configuration
    - Create entry for manage model loading
    - Create entry for manage test data
    - Create entry for manage dashboard configuration
    
    New Functionality
    - Create HTML template for upload file
    - Add function for render HTML upload template
    - Create method for predict a person by a give image
    - Flask dashboard for monitoring API
    - Visualize the prediction result drawing a rectangle in the image face

--- main.py
+++ main.py
@@ -1,81 +1,89 @@
-from datetime import datetime
+"""
+PyRecognizer loader
+"""
+import base64
 import json
 import os
-import pickle
-from pprint import pformat
 
-from Datastructure.Classifier import Classifier
-from Datastructure.Person import Person
-from utils.log import load_logger
-from utils.util import  predict,show_prediction_labels_on_image
+import flask_monitoringdashboard as dashboard
+from flask import Flask, flash, request, send_from_directory, url_for
+from werkzeug.utils import redirect, secure_filename
 
+from api.Api import upload_image_predict
+from datastructure.Classifier import Classifier
+from utils.util import load_logger, print_prediction_on_image
+
+# ===== LOAD CONFIGURATION FILE =====
 CONFIG_FILE = "conf/test.json"
-
-CFG = None
 
 with open(CONFIG_FILE) as f:
 	CFG = json.load(f)
 
 log = load_logger(CFG["logging"]["level"], CFG["logging"]["path"], CFG["logging"]["prefix"])
 
+# ===== FLASK CONFIGURATION =====
+# $(base64 /dev/urandom  | head -n 1 | md5sum | awk '{print $1}')
+SECRET_KEY = str(base64.b64encode(bytes(os.urandom(24)))).encode()
+UPLOAD_FOLDER = "/tmp/upload"
+
+app = Flask(__name__, template_folder="api/templates")
+app.secret_key = SECRET_KEY
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# =====FLASK DASHBOARD CONFIGURATION =====
+dashboard.config.init_from(file=CFG["dashboard"]["config_file"])
+dashboard.bind(app, SECRET_KEY)
+
+# ===== CLASSIFIER CONFIGURATION =====
 log.debug("Init classifier ...")
 clf = Classifier()
+# Loading mandatory data ...
 clf.trainin_dir = CFG["classifier"]["trainin_dir"]
 clf.model_path = CFG["classifier"]["model_path"]
-peoples = []
+clf.load_classifier_from_file(CFG["classifier"]["model"])
 
-log.debug("loading peoples ... {}".format(clf.trainin_dir))
-# TODO: Be sure that no Person have same name
-for people_name in os.listdir(clf.trainin_dir):
-	# Filter only folder
-	if os.path.isdir(os.path.join(clf.trainin_dir, people_name)):
-		log.debug("{0}".format(os.path.join(clf.trainin_dir, people_name)))
-		person = Person()
-		person.name = people_name
-		person.path = os.path.join(clf.trainin_dir, people_name)
-		person.init_dataset()
-		peoples.append(person)
-
-DATASET = {
-	# Image data (numpy array)
-	"X": [],
-	# Person name
-	"Y": []
-}
-
-for people in peoples:
-	log.debug(people.name)
-	for item in people.dataset["X"]:
-		DATASET["X"].append(item)
-	for item in people.dataset["Y"]:
-		DATASET["Y"].append(item)
-
-log.debug(pformat(DATASET))
-
-clf.init_specs(X_len=len(DATASET["X"]))
-clf.classifier.fit(DATASET["X"],DATASET["Y"])
-# Save the trained KNN classifier
-if clf.model_path != "" and os.path.isdir(clf.model_path):
-	time_parsed = datetime.now().strftime('%Y%m%d_%H%M%S')
-	classifier_file = 'model.clf'
-	with open(os.path.join(clf.model_path, classifier_file), 'wb') as f:
-		pickle.dump(clf.classifier, f)
+allowed_ext = ["jpg", "jpeg", "png"]
 
 
-test_folder = "/home/alessiosavi/PycharmProjects/FaceDetect/test"
+@app.route('/', methods=['GET'])
+def home():
+	"""
+	Show the html template for upload the image
+	"""
+	return upload_image_predict("upload.html")
 
-for image_file in os.listdir(test_folder):
-	full_file_path = os.path.join(test_folder, image_file)
 
-	log.info("Looking for faces in {}".format(image_file))
+@app.route('/', methods=["POST"])
+def upload():
+	"""
 
-	# Find all people in the image using a trained classifier model
-	# Note: You can pass in either a classifier file name or a classifier model instance
-	predictions = predict(full_file_path, model_path=os.path.join(clf.model_path, classifier_file))
+	:return:
+	"""
+	# check if the post request has the file part
+	if 'file' not in request.files or request.files['file'].filename == '':
+		flash('No file choosed :/', category="error")
+		return redirect(request.url)  # Return to HTML page [GET]
+	file = request.files['file']
+	# TODO: Add check on extension
+	filename = secure_filename(file.filename)
+	img_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+	file.save(img_path)
+	prediction = clf.predict(img_path)
+	print_prediction_on_image(img_path, prediction)
+	return redirect(url_for('uploaded_file', filename="predict.png"))
 
-	# Print results on the console
-	for name, (top, right, bottom, left) in predictions:
-		log.info("- Found {} at ({}, {})".format(name, left, top))
 
-	# Display results overlaid on an image
-	show_prediction_labels_on_image(os.path.join(test_folder, image_file), predictions)
+# return redirect(url_for('uploaded_file', filename=filename))
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+	"""
+
+	:param filename:
+	:return:
+	"""
+	return send_from_directory("/tmp/upload/prediction/", filename)
+
+
+if __name__ == '__main__':
+	app.run(host=CFG["network"]["host"], port=CFG["network"]["port"], threaded=True, debug=True)
