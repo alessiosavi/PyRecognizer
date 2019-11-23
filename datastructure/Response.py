636be23b6a609b747faf8372b186bf1c71b58c28
alessiosavi <Alessio.Savi@ibm.com>
2019-11-23 23:46:45 +0100

    - Enhance confidence during prediction - Clean error managing in case of no face found/broken image/unkown face - Minor optimization - Add image distortion during training/predict - Use CNN for detect point, can fall in 'out-of-memory' with old GPU

--- datastructure/Response.py
+++ datastructure/Response.py
@@ -29,15 +29,13 @@ class Response(object):
 		log = logging.getLogger()
 		log.debug("parse_data | Parsing {}".format(data))
 
-		predictions = []
-		scores = []
 		t = {}
 		if data is not None:
 			log.debug("parse_data | Data not None ...")
 			if isinstance(data, dict):
 				log.debug("parse_data | Data is a dict")
 				if "predictions" in data and "scores" in data:
-				# if predictions data["predictions"] and data["scores"]:
+					# if predictions data["predictions"] and data["scores"]:
 					log.debug("parse_data | Predictions and scores provided")
 					if isinstance(data["predictions"], list) and isinstance(data["scores"], list):
 						predictions = data["predictions"]
@@ -45,7 +43,7 @@ class Response(object):
 						if len(predictions) == len(scores):
 							log.debug("parse_data | Predictions and scores same lenght")
 						for i in range(len(predictions)):
-							t[predictions[i][0]] = scores[i]
+							t[predictions[i][0]] = 1 - scores[i]
 						log.debug("parse_data | Dict initalized -> {}".format(t))
 						return t
 
